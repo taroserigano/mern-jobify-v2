ed138d9dbfdb5dfc2f6660ea201458d1fa05d60a
Taro Serigano <taroserigano@gmail.com>
2023-12-04 12:22:36 -0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+
 import {
   HomeLayout,
   Landing,
